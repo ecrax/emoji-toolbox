ae09a8c0109f00efeecf6c71c3b89133a7304314
Leo Kling <zugecraftet@gmail.com>
2020-12-12 13:03:37 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Emoji Toolbox
+
+`emoji-toolbox` is a toolbox full of handy features for developers who want to use emojis in their code or documentation more often 😃.
+
+## Features
+
+- Replace text with a fitting emoji
+- Insert an emoji from a list of emoticons
+- Replace an ASCII emoji with its "real" emoji counterpart
+- Replace anything you have selected.
+
+## Usage
+
+### Convert
+
+- Select a text or ASCII emoji
+- Open the command palette (`ctrl`+ `shift` + `p` or `F1` or `View` -> `Command Palette...`)
+- Select an action
+  - **Convert ASCII to emoji** - converts an ASCII emoji, like ":)" to a "real" emoji like 😃
+  - **Convert text to emoji** - converts given text to a fitting emoji (wink -> 😉 | mad -> 😠)
+  - **Convert anything to emoji** - tries to convert the selected content to an emoji
+
+**Please only select one word or ASCII emoji at a time when converting. Otherwise, it will fail. (Support for this may come in the future.)**
+
+> You might get a dropdown with multiple emojis to choose from whenever multiple emojis are connected with one word. (e.g. meh can result in 😐 and 😒)
+
+### Insert
+
+- Position your cursor where you want to insert an emoji
+- Open the command palette (`ctrl`+ `shift` + `p` or `F1` or `View` -> `Command Palette...`)
+- Choose **Insert emoji**
+- Pick an emoji you like
+
+## Extension Settings
+
+I have not included any settings yet, because I have not seen the need for now.
+
+## Known Issues
+
+There are currently no issues. You may want to have a look at the GitHub issues section of this project to see if there are any.
+
+You are also very welcome to create new issues if you have any problems, find bugs, or have a feature request.
+
+## Roadmap / TODO
+
+- Support conversion of multiple ASCII emojis or words at once
+- Add images and GIFs to the README
+
+> Also all issues on GitHub 😉
+
+## Release Notes
+
+### 1.0.0
+
+Initial release of `emoji-place`
---
+++ src/extension.ts
@@ -0,0 +1,213 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  const emoticon = require("emoticon");
+
+  let emojis = new Array();
+  for (let i = 0; i < emoticon.length; i++) {
+    const element = emoticon[i]["emoji"];
+    emojis.push(element);
+  }
+
+  let disposableTwo = vscode.commands.registerCommand(
+    "emoji-place.insertEmoji",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showInformationMessage("Editor does not exist!");
+      }
+
+      const quickPick = vscode.window.createQuickPick();
+      quickPick.items = emojis.map((x: any) => ({ label: x }));
+      quickPick.onDidChangeSelection(([item]) => {
+        if (item) {
+          editor?.edit((edit) => {
+            edit.insert(editor.selection.active, item.label);
+          });
+          quickPick.dispose();
+        }
+      });
+      quickPick.onDidHide(() => quickPick.dispose());
+      quickPick.show();
+    }
+  );
+
+  let disposableFour = vscode.commands.registerCommand(
+    "emoji-place.anythingToEmoji",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showInformationMessage("Editor does not exist!");
+      }
+
+      const text = editor?.document.getText(editor.selection).trim();
+
+      let emojis = new Array();
+
+      for (let index = 0; index < emoticon.length; index++) {
+        const element = emoticon[index];
+
+        for (let j = 0; j < element["tags"].length; j++) {
+          const tags = element["tags"][j];
+
+          if (tags === text) {
+            emojis.push(element["emoji"]);
+          }
+        }
+
+        for (let j = 0; j < element["emoticons"].length; j++) {
+          const emoticons = element["emoticons"][j];
+
+          if (emoticons === text) {
+            emojis.push(element["emoji"]);
+          }
+        }
+
+        if (element["name"] === text) {
+          emojis.push(element["emoji"]);
+        }
+      }
+
+      if (emojis.length > 1) {
+        const quickPick = vscode.window.createQuickPick();
+        quickPick.items = emojis.map((x: any) => ({ label: x }));
+        quickPick.onDidChangeSelection(([item]) => {
+          if (item) {
+            editor?.edit((edit) => {
+              edit.replace(editor.selection, item.label);
+            });
+            quickPick.dispose();
+          }
+        });
+        quickPick.onDidHide(() => quickPick.dispose());
+        quickPick.show();
+      } else if (emojis.length === 0) {
+        vscode.window.showInformationMessage("Nothing to convert found");
+      } else {
+        editor?.edit((edit) => {
+          if (emojis.length !== 0) {
+            edit.replace(editor.selection, emojis[0]);
+          }
+        });
+      }
+    }
+  );
+
+  let disposableThree = vscode.commands.registerCommand(
+    "emoji-place.textToEmoji",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showInformationMessage("Editor does not exist!");
+      }
+
+      const text = editor?.document.getText(editor.selection).trim();
+
+      let emojis = new Array();
+
+      for (let index = 0; index < emoticon.length; index++) {
+        const element = emoticon[index];
+
+        for (let j = 0; j < element["tags"].length; j++) {
+          const tags = element["tags"][j];
+
+          if (tags === text) {
+            emojis.push(element["emoji"]);
+          }
+        }
+
+        if (element["name"] === text) {
+          emojis.push(element["emoji"]);
+        }
+      }
+
+      // Maybe there is more than one emoji to convert to -> show quickPick to choose -> either way replace selected emoji
+      if (emojis.length > 1) {
+        const quickPick = vscode.window.createQuickPick();
+        quickPick.items = emojis.map((x: any) => ({ label: x }));
+        quickPick.onDidChangeSelection(([item]) => {
+          if (item) {
+            editor?.edit((edit) => {
+              edit.replace(editor.selection, item.label);
+            });
+            quickPick.dispose();
+          }
+        });
+        quickPick.onDidHide(() => quickPick.dispose());
+        quickPick.show();
+      } else if (emojis.length === 0) {
+        vscode.window.showInformationMessage("No emoji found");
+      } else {
+        editor?.edit((edit) => {
+          if (emojis.length !== 0) {
+            edit.replace(editor.selection, emojis[0]);
+          }
+        });
+      }
+    }
+  );
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "emoji-place.asciiToEmoji",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showInformationMessage("Editor does not exist!");
+      }
+
+      const text = editor?.document.getText(editor.selection).trim();
+
+      let emojis = new Array();
+
+      for (let index = 0; index < emoticon.length; index++) {
+        const element = emoticon[index];
+
+        for (let j = 0; j < element["emoticons"].length; j++) {
+          const emoticons = element["emoticons"][j];
+
+          if (emoticons === text) {
+            emojis.push(element["emoji"]);
+          }
+        }
+      }
+
+      // Maybe there is more than one emoji to convert to -> show quickPick to choose -> either way replace selected emoji
+      if (emojis.length > 1) {
+        const quickPick = vscode.window.createQuickPick();
+        quickPick.items = emojis.map((x: any) => ({ label: x }));
+        quickPick.onDidChangeSelection(([item]) => {
+          if (item) {
+            editor?.edit((edit) => {
+              edit.replace(editor.selection, item.label);
+            });
+            quickPick.dispose();
+          }
+        });
+        quickPick.onDidHide(() => quickPick.dispose());
+        quickPick.show();
+      } else if (emojis.length === 0) {
+        vscode.window.showInformationMessage("No ASCII emoji detected");
+      } else {
+        editor?.edit((edit) => {
+          if (emojis.length !== 0) {
+            edit.replace(editor.selection, emojis[0]);
+          }
+        });
+      }
+    }
+  );
+
+  context.subscriptions.push(disposable);
+  context.subscriptions.push(disposableTwo);
+  context.subscriptions.push(disposableThree);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
